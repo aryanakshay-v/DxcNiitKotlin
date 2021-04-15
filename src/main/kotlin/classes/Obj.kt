8601e23fa18428495995721b42554de955ee14d2
Aryan Akshay <varyanakshay@gmail.com>
2021-04-15 10:42:27 +0530

    Object as inner class

---
+++ src/main/kotlin/classes/Obj.kt
@@ -0,0 +1,13 @@
+package classes
+
+fun rentPrice( normalday : Int, Festivaldays : Int) {
+    val dayRates = object{
+        var standard: Int = 30;
+        var festive : Int = 50
+    }
+    val total = dayRates.standard +dayRates.festive
+    println("Total price = $total")
+}
+fun main(){
+    rentPrice(12,15)
+}
