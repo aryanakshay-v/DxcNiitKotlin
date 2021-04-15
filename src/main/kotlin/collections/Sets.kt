3bd5db3b96658c5a052e18a152b477521e1ae2d3
Aryan Akshay <varyanakshay@gmail.com>
2021-04-15 16:46:03 +0530

    Sets

---
+++ src/main/kotlin/collections/Sets.kt
@@ -0,0 +1,24 @@
+package collections
+
+val issues: MutableSet<String> = mutableSetOf("issue1","issue2","issue3","issue1")
+
+fun addIssue(issue: String):Boolean{
+    if(issues.add(issue)){
+        println("issue added successfully")
+        return true;
+    }
+    else{
+        println("issue exists")
+        return false
+    }
+}
+
+class Sets {
+}
+
+fun main(){
+    println(issues)
+    println(addIssue("issue4"))
+    println(issues)
+    println(addIssue("issue2"))
+}
